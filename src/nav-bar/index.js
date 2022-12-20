01dfee31b74c4443a9469132864fc91fea4e007d
wellyngton <82477177+wellyngton-souza@users.noreply.github.com>
2022-12-20 19:06:56 -0300

    initial commit

--- src/nav-bar/index.js
+++ src/nav-bar/index.js
@@ -5,9 +5,9 @@ import brawlLogo from '../assets/brawl-stars-logo.png';
 function Navbar() {
   return (
     <div class="nav-bar">
-        <h3><a class="link-enconder" href="./user-front/user.html">Usuario</a></h3>
+        <h3><a class="link-enconder" href="">Usuario</a></h3>
         <img src={brawlLogo} width="50rem"/>
-        <h3><a class="link-enconder" href="./brawler-front/brawlers.html">Brawlers</a></h3>
+        <h3><a class="link-enconder" href="">Brawlers</a></h3>
     </div>
   );
 }
