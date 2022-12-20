c0e21f7ecafdaaa1364a5d99e64b25812b774062
wellyngton <82477177+wellyngton-souza@users.noreply.github.com>
2022-12-20 18:01:30 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import React from 'react';
-import { brawlLogo } from './assets/brawl-stars-logo.png';
+import brawlLogo from './assets/brawl-stars-logo.png';
 
 function App() {
   return (
     <div>
       <div class="nav-bar">
           <h3><a class="link-enconder" href="./user-front/user.html">Usuario</a></h3>
+          <img src={brawlLogo} width="50rem"/>
           <h3><a class="link-enconder" href="./brawler-front/brawlers.html">Brawlers</a></h3>
       </div>
     </div>
