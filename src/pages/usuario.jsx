c58a81db386df7095d0c15fce115ffb2ba78614e
MichaelFonseca <jacquelinemartins743@gmail.com>
2022-12-21 18:32:20 -0300

    usuario

---
+++ src/pages/usuario.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import Navbar from "../nav-bar"
+import Conteudo from "../conteudo-home"
+import Rodape from "../rodape"
+function Usuario () {
+
+    return(
+        <>
+        
+        <Navbar />
+        <Conteudo />
+        <Rodape />
+        </>
+    )
+
+
+}
+export default Usuario
