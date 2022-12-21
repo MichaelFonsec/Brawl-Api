aaa462faaad26c0c393fc9920406436d18f33413
wellyngton <82477177+wellyngton-souza@users.noreply.github.com>
2022-12-21 18:20:10 -0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Home from './routes/home';
+import Home from './pages/home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
