233e06f65388fe519446d045d152cc3ffa597722
wellyngton <82477177+wellyngton-souza@users.noreply.github.com>
2022-12-22 00:04:50 -0300

    initial commit

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import Conteudo from '../conteudo-home';
+import Navbar from '../nav-bar/index';
 import Rodape from '../rodape/index.jsx';
 
 function Home() {
     return (
         <>
-         
+            <Navbar />
             <Conteudo />
             <Rodape />
         </>
